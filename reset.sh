4e6c8ef088c858226121e2cdea388fdc687fb751
Koymas <koymas@localhost.localdomain>
2020-08-08 22:21:00 -0400

    fixed reset.sh to not reset the phpbb install

--- reset.sh
+++ reset.sh
@@ -1,7 +1,7 @@
 docker compose down; 
 docker system prune -a;
 docker volume prune; 
-rm -rf phpBB3;
-unzip phpBB-3.2.9.zip;
+#rm -rf phpBB3;
+#unzip phpBB-3.2.9.zip;
 docker-compose up -d; 
 docker-compose exec apache chown -Rv www-data:www-data /var/www/html/;
